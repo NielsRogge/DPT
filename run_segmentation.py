4882f37f20935f9958d2c7fcd264cbd3f8a41a90
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-02-24 17:38:32 +0100

    Tweak segmentation inference script

--- run_segmentation.py
+++ run_segmentation.py
@@ -12,7 +12,11 @@ import util.io
 
 from torchvision.transforms import Compose
 from dpt.models import DPTSegmentationModel
-from dpt.transforms import Resize, NormalizeImage, PrepareForNet
+from dpt.transforms import NormalizeImage, PrepareForNet
+
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize
+import requests
+from PIL import Image
 
 
 def run(input_path, output_path, model_path, model_type="dpt_hybrid", optimize=True):
@@ -49,21 +53,27 @@ def run(input_path, output_path, model_path, model_type="dpt_hybrid", optimize=T
             False
         ), f"model_type '{model_type}' not implemented, use: --model_type [dpt_large|dpt_hybrid]"
 
-    transform = Compose(
-        [
-            Resize(
-                net_w,
-                net_h,
-                resize_target=None,
-                keep_aspect_ratio=True,
-                ensure_multiple_of=32,
-                resize_method="minimal",
-                image_interpolation_method=cv2.INTER_CUBIC,
-            ),
-            NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            PrepareForNet(),
-        ]
-    )
+    # transform = Compose(
+    #     [
+    #         Resize(
+    #             net_w,
+    #             net_h,
+    #             resize_target=None,
+    #             keep_aspect_ratio=True,
+    #             ensure_multiple_of=32,
+    #             resize_method="minimal",
+    #             image_interpolation_method=cv2.INTER_CUBIC,
+    #         ),
+    #         NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    #         PrepareForNet(),
+    #     ]
+    # )
+
+    transform = Compose([
+        Resize((net_h, net_w)),
+        ToTensor(),
+        Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
 
     model.eval()
 
@@ -87,20 +97,24 @@ def run(input_path, output_path, model_path, model_type="dpt_hybrid", optimize=T
         print("  processing {} ({}/{})".format(img_name, ind + 1, num_images))
 
         # input
-        img = util.io.read_image(img_name)
-        img_input = transform({"image": img})["image"]
+        #img = util.io.read_image(img_name)
+        #img_input = transform({"image": img})["image"]
+
+        url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+        img = Image.open(requests.get(url, stream=True).raw)
+        sample = transform(img).to(device).unsqueeze(0)
 
         # compute
         with torch.no_grad():
-            sample = torch.from_numpy(img_input).to(device).unsqueeze(0)
-            if optimize == True and device == torch.device("cuda"):
-                sample = sample.to(memory_format=torch.channels_last)
-                sample = sample.half()
+            # sample = torch.from_numpy(img_input).to(device).unsqueeze(0)
+            # if optimize == True and device == torch.device("cuda"):
+            #     sample = sample.to(memory_format=torch.channels_last)
+            #     sample = sample.half()
 
             out = model.forward(sample)
 
             prediction = torch.nn.functional.interpolate(
-                out, size=img.shape[:2], mode="bicubic", align_corners=False
+                out, size=img.size[::-1], mode="bicubic", align_corners=False
             )
             prediction = torch.argmax(prediction, dim=1) + 1
             prediction = prediction.squeeze().cpu().numpy()
